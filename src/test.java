13569cc3aeb61b019f52e3fa3d4825915d4847b5
t13cs005 <t13cs005@yamanashi.ac.jp>
2015-11-17 17:43:15 +0900

    hennkou

--- src/test.java
+++ src/test.java
@@ -5,9 +5,9 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int i = 10;
+		int i = 14;
 		int j = 3;
-		System.out.println(i);
+		System.out.println(i+j);
 		
 	}
 
